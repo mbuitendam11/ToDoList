45f9294cac3f7abacada8e0cce23cc866864e00e
Mathew Buitendam <mbuitendam@aspenmedical.com>
2024-04-02 08:19:05 +1100

    Merge update-tickets, working as intended

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "os.environ.get('FLASK_KEY')"
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 Bootstrap5(app)
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sqlite3.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
